4187eab3cd636f6107abf8834e1a5c665f3eca19
Berkanktk <berkank@live.dk>
2022-02-14 03:01:23 +0100

    Added subdomain enumeration script

---
+++ Tools/Subdomain Enumeration/README.md
@@ -0,0 +1,5 @@
+# How to use
+Run: `python3 Subdomain-enumeration.py <link here>` 
+
+Example:
+`python3 Subdomain-enumeration.py berkankutuk.dk`
---
+++ Tools/Subdomain Enumeration/Subdomain-enumeration.py
@@ -0,0 +1,17 @@
+import requests 
+import sys 
+
+sub_list = open("subdomains.txt").read() 
+subdoms = sub_list.splitlines()
+
+for sub in subdoms:
+    sub_domains = f"http://{sub}.{sys.argv[1]}" 
+
+    try:
+        requests.get(sub_domains)
+    
+    except requests.ConnectionError: 
+        pass
+    
+    else:
+        print("Valid domain: ", sub_domains)
